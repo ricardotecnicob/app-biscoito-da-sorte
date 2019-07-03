f0d3e7bcb7c98bc00486225dd0a13b06ecff4aa1
ricardoalves <ricardoalves@bcomp.com>
2019-07-03 19:58:52 -0300

    App Biscoito Da Sorte

--- App.js
+++ App.js
@@ -1,6 +1,46 @@
 import React,{ Component } from 'react';
-import { View, Text, Button, StyleSheet, Image, TextInput,Alert,ImageBackground } from 'react-native';
+import { View, Text, Button, StyleSheet, Image, TextInput,Alert,ImageBackground, TouchableOpacity } from 'react-native';
 
+class Botao extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {};
+    this.styles = StyleSheet.create({
+      botao:{
+        width:250,
+        height:50,
+        borderWidth: 2,
+        borderColor: props.color,
+        backgroundColor: 'transparent',
+        borderRadius: 25
+      },
+      botaoArea: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      botaoText: {
+        color: props.color,
+        fontSize: 14,
+        fontWeight: 'bold'
+      }
+    });
+  }
+
+
+
+  render(){
+    return(
+      <TouchableOpacity style={this.styles.botao} onPress={this.props.onPress} >
+          <View style={this.styles.botaoArea}>
+            <Text style={this.styles.botaoText}  >{this.props.text}</Text>
+          </View>
+      </TouchableOpacity>
+    );
+  }
+}
 
 export default class PrimeiroProjeto extends Component{
 
@@ -8,73 +48,38 @@ export default class PrimeiroProjeto extends Component{
     super(props);
 
     this.state = {
-      consumido: 0,
-      status: 'Ruim',
-      pct: 0
+      texto: "Frase do Dia..."
     };
 
-    this.addCopo = this.addCopo.bind(this);
-    this.atualizar = this.atualizar.bind(this);
+    this.frases = [
+      'As montanhas da vida não existem apenas para que você chegue no topo, mas para que você aprenda o valor da escalada.',
+      'A vida pode até te derrubar, mas é você quem escolhe a hora de se levantar.',
+      'É melhor ser verdadeiro e solitário do que viver em falsidade e estar sempre acompanhado.',
+      'Ser feliz nao é ter uma vida perfeita, mas sim reconhecer que vale a pena viver apesar de todos os desafios e perdas.',
+      'Aprendi que não devo me importar com comentários que não vão mudar minha vida.',
+      'A vida tem sons, que pra gente ouvir precisa aprender a começar de novo. É como tocar o mesmo violão e nele compor uma nova canção.'
+    ];
 
-  }
-  
-  atualizar(){
-    let s = this.state; 
-    s.pct =  Math.floor(((s.consumido/2000)*100));
-
-    if(s.pct >= 100 ){
-      s.status = "Bom";
-    }else{
-      s.status = "Ruim";
-    }
-
-    this.setState(s);
+    this.quebrarBiscoito = this.quebrarBiscoito.bind(this);
 
   }
 
-  addCopo(){
+  quebrarBiscoito(){
     let s = this.state;
-    s.consumido += 200;
-    this.setState(s);
 
-    this.atualizar();
+    let r = Math.floor(Math.random() * this.frases.length);
+
+    s.texto = this.frases[r];
+    this.setState(s);
   }
 
   render(){
     return(
 
       <View style={styles.body} >
-        <ImageBackground source={require('./images/waterbg.png')} style={styles.bgimage} >
-          <View>
-            <View style={styles.infoArea}>
-
-              <View style={styles.area}>
-                <Text style={styles.areaTitulo}>Meta</Text>
-                <Text style={styles.areaDado}>2000ml</Text>
-              </View>
-
-              <View style={styles.area}>
-                <Text style={styles.areaTitulo}>Consumido</Text>
-                <Text style={styles.areaDado}>{this.state.consumido}ml</Text>
-              </View>
-
-              <View style={styles.area}>
-                <Text style={styles.areaTitulo}>Status</Text>
-                <Text style={styles.areaDado}>{this.state.status}</Text>
-              </View>
-
-            </View>
-
-            <View style={styles.pctArea}>
-                <Text style={styles.pctText}>{this.state.pct}%</Text>
-            </View>
-
-            <View style={styles.btnArea}>
-                <Button  title="Beber 200ml" onPress={this.addCopo} />
-            </View>
-
-          </View>
-        </ImageBackground>
+          <Image source={require('./images/cookie.png')} />
+          <Text style={styles.texto} >"{this.state.texto}"</Text>
+          <Botao color="#B59619" text="Quebrar Biscoito" onPress={this.quebrarBiscoito} />
       </View>
 
     );
@@ -83,43 +88,18 @@ export default class PrimeiroProjeto extends Component{
 }
 
 const styles = StyleSheet.create({
-  body:{
+  body: {
     flex: 1,
-    paddingTop: 30
-  },
-  bgimage:{
+    paddingTop: 30,
     flex: 1,
-    width: null
-  },
-  infoArea: {
-    flex: 1,
-    flexDirection: 'row',
-    marginTop: 50
-  },
-  area:{
-    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center'
   },
-  areaTitulo:{
-    color: '#45b2fc'
-  },
-  areaDado: {
-    color: '#2b4274',
-    fontSize: 15,
-    fontWeight: 'bold'
-    
-  },
-  pctArea:{
-    marginTop: 190,
-    alignItems: 'center'
-  },
-  pctText:{
-    fontSize: 70,
-    color: "#fff",
-    backgroundColor: 'transparent'
-  },
-  btnArea: {
-    marginTop: 30,
-    alignItems: 'center'
+  texto:{
+    fontSize: 17,
+    color: '#B59619',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
   }
 });
